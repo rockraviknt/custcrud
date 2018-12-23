3be3d7fb2b5e365582d2950adea8ebdd012f13ee
Ravi Kant-GGN <ravi_kant-ggn@external.mckinsey.com>
2018-12-24 00:25:53 +0530

    Modified Test Utility

--- src/test/java/com/org/techxenzia/utility/AppTestUtility.java
+++ src/test/java/com/org/techxenzia/utility/AppTestUtility.java
@@ -7,8 +7,8 @@ public class AppTestUtility {
 	 public static String asJsonString(final Object obj) {
 	        try {
 	            return new ObjectMapper().writeValueAsString(obj);
-	        } catch (Exception e) {
-	            throw new RuntimeException(e);
+	        } catch (Exception exp) {
+	            throw new RuntimeException(exp);
 	        }
 	    }
 }
